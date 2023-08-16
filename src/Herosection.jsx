e721e5bfbe93c23f904ebf24eb94485563a0c88e
MarkMoses27 <markmoses817@gmail.com>
2023-08-16 20:18:50 +0300

    Add images and Hero section CSS

--- src/Herosection.jsx
+++ src/Herosection.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from 'react';
+import "./Herosection.css";
+
 
 function Herosection() {
+  const handleHireClick = () => {
+    window.location.href = "mailto:markmoses817@gmail.com?subject=Hiring Inquiry";
+  };
+
+  const handleLearnClick = () => {
+    window.open("https://www.example.com", "_blank");
+  };
+
   return (
-    <div>Herosection</div>
-  )
+    <div className='Home-page'>
+       
+          <h2>Hello there,</h2>
+          <h1> I'm Mark <span>Moses</span></h1>
+          <p>I'm a full-stack developer</p>
+          <div className="cta-buttons">
+            <button className="hire-button" onClick={handleHireClick}>Hire Me</button>
+            <button className="learn-button" onClick={handleLearnClick}>Learn More</button>
+          </div>
+        </div>
+  );
 }
 
-export default Herosection
+export default Herosection;
